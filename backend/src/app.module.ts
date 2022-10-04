8ddcf3fee06a4438e58e60b9a5a2bfb609625bf9
cancan007 <shota.moue0211@gmail.com>
2022-10-05 01:25:41 +0900

    try to deploy on heroku

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -11,7 +11,7 @@ import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
 
 const uri = process.env.NODE_ENV === 
-"production" ? `mongodb+srv://shota:${process.env.MONGODB_PASSWORD}@cluster-free.mmct5.mongodb.net/?retryWrites=true&w=majority`
+"production" ? `mongodb+srv://shota:${process.env.MONGODB_PASSWORD}@cluster-free.mmct5.mongodb.net/?retryWrites=true&w=majority/visionswipe-buildings`
              : 'mongodb://localhost:27017/visionswipe'
 @Module({
   imports: [
